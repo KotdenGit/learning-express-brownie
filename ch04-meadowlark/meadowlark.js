33cffbf7e1523486238c9606ecb6def43ffa1e2a
Yury V <kotden09@gmail.com>
2026-09-03 15:42:13 +0300

    feat: refactor handlers into separate file and update routes

--- ch04-meadowlark/meadowlark.js
+++ ch04-meadowlark/meadowlark.js
@@ -1,12 +1,12 @@
 const express = require("express");
-
 const { engine: expressHandlebars } = require("express-handlebars");
 
-const fortune = require("./lib/fortune");
+//const fortune = require("./lib/fortune");
+const handlers = require("./lib/handlers");
 
 const app = express();
 
-// Настройка механизма представлений Handlebars.
+// configure Handlebars view engine
 app.engine(
   "handlebars",
   expressHandlebars({
@@ -16,29 +16,19 @@ app.engine(
 
 app.set("view engine", "handlebars");
 
-app.use(express.static(__dirname + "/public"));
-
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => res.render("home"));
+app.use(express.static(__dirname + "/public"));
 
-app.get("/about", (req, res) => {
-  // const randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-  res.render("about", { fortune: fortune.getFortune() });
-});
+app.get("/", handlers.home);
+
+app.get("/about", handlers.about);
 
 // custom 404 page
-app.use((req, res) => {
-  res.status(404);
-  res.render("404");
-});
+app.use(handlers.notFound);
 
 // custom 500 page
-app.use((err, req, res, next) => {
-  console.error(err.message);
-  res.status(500);
-  res.render("500");
-});
+app.use(handlers.serverError);
 
 app.listen(port, () =>
   console.log(
