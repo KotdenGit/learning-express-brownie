9363aec147d53640397c4940cfdb5c0c5ce02926
Yury V <kotden09@gmail.com>
2026-09-07 18:19:05 +0300

    feat: update ESLint configuration to include Jest globals and adjust rules

--- ch04-meadowlark/eslint.config.mjs
+++ ch04-meadowlark/eslint.config.mjs
@@ -3,6 +3,12 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: { ...globals.browser, ...globals.jest } },
+    rules: { "no-console": "off" },
+  },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
 ]);
