9c45f1d1f28f86ca05c2098e3f741bbc9b5ea4de
Yury V <kotden09@gmail.com>
2026-09-14 16:43:55 +0300

    refactor: standardize code formatting and style across response handlers

--- ch06-response/00-echo-headers.js
+++ ch06-response/00-echo-headers.js
@@ -1,12 +1,12 @@
-app.get("/headers", (req, res) => {
-  res.type("text/plain");
-  const headers = Object.entries(req.headers).map(
-    ([key, value]) => `${key}: ${value}`,
-  );
-  res.send(headers.join("\n"));
-});
+const express = require('express')
+const app = express()
 
-const port = process.env.PORT || 3000;
-app.listen(port, () =>
-  console.log(`\nnavigate to http://localhost:${port}/headers\n`),
-);
+app.get('/headers', (req, res) => {
+  res.type('text/plain')
+  const headers = Object.entries(req.headers)
+    .map(([key, value]) => `${key}: ${value}`)
+  res.send(headers.join('\n'))
+})
+
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`\nnavigate to http://localhost:${port}/headers\n`))
