b9b11a468ee9f20add6dfca288f62862ea35f9b7
Yury V <kotden09@gmail.com>
2026-09-14 16:44:21 +0300

    feat: add greeting view with cookie and session support

--- ch06-response/03-different-response-code.js
+++ ch06-response/03-different-response-code.js
@@ -1,19 +1,15 @@
-const express = require("express");
-const expressHandlebars = require("express-handlebars");
-const app = express();
+const express = require('express')
+const expressHandlebars = require('express-handlebars')
+const app = express()
 
 // the following is needed to use views
-app.engine("handlebars", expressHandlebars({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 
 // see the views/error.hbs file for the contents of this view
-app.get("/error", (req, res) => res.status(500).render("error"));
+app.get('/error', (req, res) => res.status(500).render('error'))
 
-app.get("*", (req, res) =>
-  res.send('Check out our <a href="/error">error</a> page!'),
-);
+app.get('*', (req, res) => res.send('Check out our <a href="/error">error</a> page!'))
 
-const port = process.env.PORT || 3000;
-app.listen(port, () =>
-  console.log(`\nnavigate to http://localhost:${port}/error\n`),
-);
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`\nnavigate to http://localhost:${port}/error\n`))
